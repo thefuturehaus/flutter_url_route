5fda6751755adc891e2365de6359e639265efcb1
Kevin Morey <kevin@futurehaus.com>
2020-06-09 14:13:29 -0500

    Fix breaking changes from flutter

--- lib/url_route.dart
+++ lib/url_route.dart
@@ -1,39 +1,29 @@
 library url_route;
+
 import 'package:flutter/material.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
-
-
-
-class UrlRoute{
+class UrlRoute {
   static Map<String, WidgetBuilder> routes;
 
   Route<dynamic> generator(RouteSettings settings) {
-    if(routes[settings.name] != null){
+    if (routes[settings.name] != null) {
       WidgetBuilder builder = routes[settings.name];
-      return MaterialPageRoute(builder:builder,settings: settings);
+      return MaterialPageRoute(builder: builder, settings: settings);
     }
     Uri url = Uri.parse(settings.name);
-    if(routes[url.path] != null){
+    if (routes[url.path] != null) {
       WidgetBuilder builder = routes[url.path];
-      RouteSettings newSetting = RouteSettings(name: url.path,arguments: url.queryParameters,isInitialRoute: settings.isInitialRoute);
-      return MaterialPageRoute(builder: builder,settings: newSetting);
+      RouteSettings newSetting = RouteSettings(
+          name: url.path,
+          arguments: url.queryParameters);
+      return MaterialPageRoute(builder: builder, settings: newSetting);
     }
     print("未找到的路由！");
     return RouteMatch().route;
   }
-
 }
-
-
 
 class RouteMatch {
   RouteMatch({this.route});
   final Route<dynamic> route;
-
 }
-
